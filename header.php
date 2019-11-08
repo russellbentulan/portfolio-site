681a40fa7062bfa0c65983113ae3eeb9a066a6f8
Russell Bentulan <russellbentulan@gmail.com>
2019-11-08 14:25:25 -0500

    changing font styles 🐧🐧

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+  
+  <link href="https://fonts.googleapis.com/css?family=Chewy|Lato:400,400i,700&display=swap" rel="stylesheet">
+
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
   <?php wp_head(); ?>
@@ -34,12 +36,6 @@
         </span>
 
       </label>
-      <!-- <button class="menu-button">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span class="visually-hidden">Menu</span>
-      </button> -->
 
       <?php wp_nav_menu( array(
         'theme_location' => 'primary',
